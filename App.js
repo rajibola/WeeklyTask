9fca02ed16cc74ce5cdaee0880ad010be29eddc4
Ridwan Ajibola <rajibola1997@gmail.com>
2020-06-18 04:49:22 -0700

    created a seperate folder for Task1

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
+import Profiles from "./myData.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={Profiles}
+        renderItem={({ item }) => (
+          <View style={styles.view}>
+            <Image style={styles.image} source={item.avatar} />
+
+            <View style={styles.details}>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text>{item.topping}</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -12,8 +25,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
   },
+
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: "cover",
+    borderRadius: 50,
+    marginRight: 15,
+  },
+
+  view: {
+    flexDirection: "row",
+    margin: 10,
+    alignItems: "center",
+  },
+
+  details: { justifyContent: "center" },
+
+  name: { fontSize: 25, fontWeight: "bold", marginBottom: 10 },
 });
---
+++ WeeklyTask/Task1/myData.js
@@ -0,0 +1,33 @@
+var Profiles = [
+  {
+    name: "Mark Zukerberg",
+    title: "Founder and CEO of Facebook, Inc.",
+    avatar: require("./images/Mark.jpg"),
+    id: "1",
+  },
+  {
+    name: "Donald Trump",
+    title: "45th President of the United States",
+    avatar: require("./images/trump.png"),
+    id: "2",
+  },
+  {
+    name: "Lionel Messi",
+    title: "Six times winner of FIFA's Player of the Year",
+    avatar: require("./images/Messi.png"),
+    id: "3",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    title: "Five times winner of FIFA's Player of the Year",
+    avatar: require("./images/ronaldo.jpg"),
+    id: "4",
+  },
+  {
+    name: "Michael Jackson",
+    title: "King of Pop",
+    avatar: require("./images/michael.jpg"),
+    id: "5",
+  },
+];
+export default Profiles;
