bb3bba51d3ab15f554bce96c652760a4fd23daaf
Ridwan Ajibola <rajibola1997@gmail.com>
2020-06-19 14:39:15 -0700

    implemented task two
    Handling back button in react native

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, BackHandler, Alert } from "react-native";
 
-export default function App() {
-  return <View style={styles.container}></View>;
+export default class App extends Component {
+  componentWillMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+  }
+
+  onBackPress = () => {
+    Alert.alert(
+      "Exit from App",
+      "Do you want to exit from App",
+      [
+        { text: "Yes", onPress: () => BackHandler.exitApp() },
+        { text: "No", onPress: () => console.log("No Pressed") },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: 25, textAlign: "center" }}>
+          Press back button to see the alert message
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
